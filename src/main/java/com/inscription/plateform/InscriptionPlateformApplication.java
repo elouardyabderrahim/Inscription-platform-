e5be0d925a610a569344f9be9c4788efcca7f81f
abderrahim elouardy <elouardyabderrahim07@gmail.com>
2022-11-24 10:06:25 +0100

    initial commit

---
+++ src/main/java/com/inscription/plateform/InscriptionPlateformApplication.java
@@ -0,0 +1,13 @@
+package com.inscription.plateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionPlateformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscriptionPlateformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inscription/plateform/InscriptionPlateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inscription.plateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscriptionPlateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
