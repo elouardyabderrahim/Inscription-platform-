e2461abab15e202d0cb763469a27fd47660f860a
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-25 11:47:41 +0100

    Ameliorate DTO commit

--- src/main/java/com/inscription/plateform/dto/UserDto.java
+++ src/main/java/com/inscription/plateform/dto/UserDto.java
@@ -1,4 +1,21 @@
 package com.inscription.plateform.dto;
 
+import com.inscription.plateform.entity.Role;
+import lombok.Data;
+
+import javax.persistence.Column;
+import java.util.Collection;
+
+@Data
 public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastname;
+    private String userName;
+    private String email;
+    private String password;
+
+    private Collection<Role> roles;
+
 }
